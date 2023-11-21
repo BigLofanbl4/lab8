fd43dae3f97464c7d172650e7097a7eebc96d47b
BigLofanbl4 <smallofanbl4@gmail.com>
2023-11-21 19:50:27 +0300

    Lab8

---
+++ progs/1.py
@@ -0,0 +1,7 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+if __name__ == "__main__":
+    julySales = (15000, 7300)
+    augustSales = (3456, 8900)
+    print(f"Sum of sales in July and August = {sum(julySales) + sum(augustSales)}")
