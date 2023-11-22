03dba8c62f7fe8f0b82822aadc491c2c5329ed3a
BigLofanbl4 <smallofanbl4@gmail.com>
2023-11-22 12:37:50 +0300

    Refactor 1.py

--- progs/1.py
+++ progs/1.py
@@ -5,17 +5,15 @@ import random
 
 if __name__ == "__main__":
     july_sales = [
-        (random.randint(100, 1000) for i in range(31)),
-        (random.randint(100, 1000) for i in range(31))
+        (random.randint(100, 1000), 
+        (random.randint(100, 1000))) for _ in range(31)
     ]
     august_sales = [
-        (random.randint(100, 1000) for i in range(31)),
-        (random.randint(100, 1000) for i in range(31))
+        (random.randint(100, 1000), 
+        (random.randint(100, 1000))) for _ in range(31)
     ]
     res = (
-        sum(july_sales[0])
-        + sum(july_sales[1])
-        + sum(august_sales[0])
-        + sum(august_sales[1])
+        sum([sum(i) for i in july_sales]) + 
+        sum([sum(i) for i in august_sales])
     )
     print(f"Sum of sales in July and August = {res}")
