b746f3ebf93d8bfc25595915c97badc1e149c699
BigLofanbl4 <smallofanbl4@gmail.com>
2023-11-22 12:47:42 +0300

    2

--- progs/1.py
+++ progs/1.py
@@ -5,15 +5,10 @@ import random
 
 if __name__ == "__main__":
     july_sales = [
-        (random.randint(100, 1000), 
-        random.randint(100, 1000)) for _ in range(31)
+        (random.randint(100, 1000), random.randint(100, 1000)) for _ in range(31)
     ]
     august_sales = [
-        (random.randint(100, 1000), 
-        random.randint(100, 1000)) for _ in range(31)
+        (random.randint(100, 1000), random.randint(100, 1000)) for _ in range(31)
     ]
-    res = (
-        sum([sum(i) for i in july_sales]) + 
-        sum([sum(i) for i in august_sales])
-    )
+    res = sum([sum(i) for i in july_sales]) + sum([sum(i) for i in august_sales]) 
     print(f"Sum of sales in July and August = {res}")
